ca8651cbc6efef295119e033db41720e3cd864bd
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-02-03 03:09:44 -0800

    cleaning up the codebase

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -7,9 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -35,22 +33,17 @@ public class CategoryController {
 
    @DeleteMapping("/admin/categories/{categoryId}")
    public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId) {
-        try{
+
             String status= categoryService.deleteCategory(categoryId);
             return new ResponseEntity<>(status, HttpStatus.OK);
-        }catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(),e.getStatusCode());
-        }
 
    }
   @PutMapping(("/public/categories/{categoryId}"))
-  public ResponseEntity<String> updateCategory(@RequestBody Category category,
+  public ResponseEntity<String> updateCategory(@Valid @RequestBody Category category,
                                                @PathVariable Long categoryId) {
-        try{
+
             Category savedCategory= categoryService.updateCategory(category,categoryId);
             return new ResponseEntity<>("Category with category id : " + categoryId +" is updated", HttpStatus.OK);
-        }catch (ResponseStatusException e){
-            return new ResponseEntity<>(e.getReason(),e.getStatusCode());
-        }
+
    }
 }
--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -5,20 +5,14 @@ import com.ecommerce.project.exception.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
-
-
-    private Long nextId = 1L;
 
     @Autowired
     private CategoryRepository categoryRepository;
