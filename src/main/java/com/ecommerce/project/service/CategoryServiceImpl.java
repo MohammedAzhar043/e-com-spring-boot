30b38c97dc02a8a822ae9452084c3b6e0bf09f16
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-02-03 06:08:03 -0800

    intriduction and usage of ModelMapper to conver the category class to categoryDTO

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -3,13 +3,16 @@ package com.ecommerce.project.service;
 import com.ecommerce.project.exception.APIException;
 import com.ecommerce.project.exception.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.payload.CategoryDTO;
 import com.ecommerce.project.payload.CategoryResponse;
 import com.ecommerce.project.repository.CategoryRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -18,6 +21,9 @@ public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public CategoryResponse getAllCategories() {
 
@@ -25,7 +31,13 @@ public class CategoryServiceImpl implements CategoryService {
         if (AllCategories.isEmpty()) {
             throw new APIException("No Category found, please try to insert the new category");
         }
-        return AllCategories;
+        /*converting category to categoryDTO by using ModelMapper*/
+        List<CategoryDTO> categoryDTOS = AllCategories.stream().
+                map(category -> modelMapper.map(category, CategoryDTO.class))
+                .toList();
+        CategoryResponse categoryResponse = new CategoryResponse();
+        categoryResponse.setContent(categoryDTOS);
+        return categoryResponse;
     }
 
     @Override
