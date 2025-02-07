158891095e552ba513f494eec9bf8a38030aa511
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-02-06 21:49:55 -0800

    update product

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -41,4 +41,11 @@ public class ProductController {
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
 
+    @PutMapping(("/admin/products/{productId}"))
+    public ResponseEntity<ProductDTO> updateProduct(@RequestBody Product product,
+                                                    @PathVariable Long productId){
+        ProductDTO updateProductDTO =  productService.updateProduct(productId,product);
+        return new ResponseEntity<>(updateProductDTO,HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -92,4 +92,24 @@ public class ProductServiceImpl implements ProductService {
         return productResponse;
 
     }
+
+    @Override
+    public ProductDTO updateProduct(Long productId, Product product) {
+        //get the existing product
+        Product productFromDb = productRepository.findById(productId).
+                orElseThrow(() -> new ResourceNotFoundException("product", "productId", productId));
+        //update the product info with user shared
+        productFromDb.setProductName(product.getProductName());
+        productFromDb.setDescription(product.getDescription());
+        productFromDb.setQuantity(product.getQuantity());
+        productFromDb.setDiscount(product.getDiscount());
+        productFromDb.setPrice(product.getPrice());
+        double specialPrice = product.getPrice() -
+                ((product.getDiscount() * 0.01) * product.getPrice());
+        productFromDb.setSpecialPrice(specialPrice);
+
+        //save to data base
+        Product savedProduct = productRepository.save(productFromDb);
+        return modelMapper.map(savedProduct,ProductDTO.class);
+    }
 }
