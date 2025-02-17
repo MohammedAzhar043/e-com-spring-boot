1fbf948d4024bd7903cefaac5d798621d7daa80b
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-02-16 21:27:56 -0800

    signup request

--- src/main/java/com/ecommerce/project/repository/UserRepository.java
+++ src/main/java/com/ecommerce/project/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.ecommerce.project.repository;
 
 import com.ecommerce.project.model.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +14,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByUserName(String username);
+
+    Boolean existsByUserName(String username);
+    Boolean existsByEmail(String email);
 }
