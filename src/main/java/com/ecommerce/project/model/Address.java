114b0fa7db8fbd589e5ae82677451bb64cd9353c
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-02-18 21:22:36 -0800

    creating the addresses in the controller and service and its implimentation

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -6,10 +6,7 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Table(name="addresses")
@@ -46,9 +43,10 @@ public class Address {
     @Size(min=6, message = "pincode name must be 6 characters")
     private String pincode;
 
-    @ToString.Exclude
-    @ManyToMany(mappedBy = "addresses")
-    private List<User> users= new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Address(String street, String buildingName, String city, String state, String country, String pincode) {
         this.street = street;
