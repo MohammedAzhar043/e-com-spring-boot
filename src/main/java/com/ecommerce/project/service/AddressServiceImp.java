bf381a0eaa11553974bd55039a16f4ac79fe749f
MohammedAzhar043 <146701964+MohammedAzhar043@users.noreply.github.com>
2025-02-18 21:33:43 -0800

    Getting Addresses

--- src/main/java/com/ecommerce/project/service/AddressServiceImp.java
+++ src/main/java/com/ecommerce/project/service/AddressServiceImp.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressServiceImp implements AddressService {
@@ -33,4 +34,14 @@ public class AddressServiceImp implements AddressService {
 
         return modelMapper.map(savedAddress, AddressDTO.class);
     }
+
+    @Override
+    public List<AddressDTO> getAddresses() {
+        List<Address> addresses = addressRepository.findAll();
+        List<AddressDTO> addressDTOs = addresses.stream().map(address -> modelMapper.map(address, AddressDTO.class))
+                .collect(Collectors.toList());
+        return addressDTOs;
+    }
+
+
 }
